500cb8dc5a9bdb0887622c6208086dba102f7a03
nguyentuyengiangbn@gmail.com <nguyentuyengiangbn@gmail.com>
2018-02-02 21:37:16 +0700

    Blog demo

---
+++ BlogUnelti/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog Uneti | Đại học kinh tế kỹ thuật công nghiệp</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="js/slick/slick-1.8.0/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="js/slick/slick-1.8.0/slick/slick-theme.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/slick/slick-1.8.0/slick/slick.min.js"></script>
+	<script src="js/jmain.js"></script>
+</head>
+<body>
+	<div id="header">
+		<div class="header-info">
+			<div class="tab">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li style="background:#4099FF;"><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li style="background:#D34836;"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					<li style="background:#2ABB67;"><a href="#"><i class="fa fa-angle-up"></i></a></li>
+					<li style="background:#8E44AD;"><a href="#"><i class="fa fa-retweet"></i></a></li>
+			</div>
+		</div><!--header-info-->
+		<div class="logo">
+			<a href="#" title="Blog Uneti | Đại học kinh tế kỹ thuật công nghiệp"><img src="images/logo.png"></a>
+		</div><!--logo-->
+		<div class="header-search">
+			<form method="post" class="form-search">
+				<input type="search" name="search" placeholder="Bạn tìm gì nào?">
+				<button type="submit" class="btn-search"><i class="fa fa-search"></i></button>
+			</form>
+		</div><!--header-search-->
+	</div><!--header-->
+
+	<div id="slider">
+		<div id="blog-slider">
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+			<div class="slider-content">
+				<div class="slider-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--slider-content-img-->
+				<div class="slider-content-title">
+					<a href="#">Bài viết hot 1</a>
+				</div><!--slider-content-title-->
+			</div><!--slider-content-->
+		</div><!--blog-slider-->
+	</div><!--slider-->
+
+	<div id="menu">
+		<div class="menu-nav">
+			<ul>
+				<li><a href="index.php"><i class="fa fa-home"></i></a></li>
+				<li><a href="#">Lập trình</a></li>
+				<li><a href="#">Tài liệu</a></li>
+				<li><a href="#">Tin tức</a></li>
+			</ul>
+		</div><!--menu-nav-->
+	</div><!--menu-->
+	<div style="clear:left"></div>
+
+	<div id="blog-main">
+		<div class="blog-main-left">
+			<div class="left-category">
+				<div class="list-title">
+					<i class="fa fa-align-justify "></i> Chuyên mục
+				</div><!--list-title-->
+				<ul>
+					<li><a href="#">Lập trình <i style="color:black;float:right;line-height:40px;margin-right:20px" class="fa fa-angle-down"></i></a></li>
+					<li><a href="#">Tài liệu <i style="color:black;float:right;line-height:40px;margin-right:20px" class="fa fa-angle-down"></i></a></li>
+					<li><a href="#">Tin tức <i style="color:black;float:right;line-height:40px;margin-right:20px" class="fa fa-angle-down"></i></a></li>
+				</ul>
+			</div><!--left-category-->
+
+			<div class="left-category">
+				<div class="list-title">
+					<i class="fa fa-user"></i> Đăng nhập
+				</div><!--list-title-->
+				<form method="post" class="form-login">
+					<label>
+						<h4>Tên đăng nhập</h4>
+						<input type="text" name="username" placeholder="Nhập tên đăng nhập">
+					</label>
+					<label>
+						<h4>Mật khẩu</h4>
+						<input type="password" name="password" placeholder="Nhập mật khẩu">
+					</label><br>
+					<button type="submit">Đăng nhập</button>
+				</form>
+			</div><!--left-category-->
+
+			<div class="left-category">
+				<div class="list-title">
+					<i class="fa fa-user"></i> Video dạy học
+				</div><!--list-title-->
+				
+			</div><!--left-category-->
+		</div><!--blog-main-left-->
+
+		<div class="blog-main-right">
+			<div class="list-title">
+				<i class="fa fa-plus"></i> Cập nhật hàng ngày
+			</div><!--list-title-->
+			<div class="main-right-content">
+				<div class="main-right-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--main-right-content-img-->
+
+				<div class="main-right-content-nd">
+					<div class="main-right-content-nd-title">
+						<a href="#">Bài viết 1</a>
+					</div><!--main-right-content-nd-title-->
+					<div class="main-right-content-nd-excerpt">
+						<p>rtyuioptyuiop[ytuiopytuioytuioytuio
+						rtyuioptyuiop[ytuiopytuioytuioytuiortyuioptyuiop[ytuiopytuioytuioytuiortyuioptyuiop[ytuiopytuioytuioytuio</p>
+					</div><!--main-right-content-nd-excerpt-->
+					<div class="main-right-button">
+						<button type="submit" class="btn-them"><a href="#">Đọc thêm <i class="fa fa-angle-double-right"></i></a></button>
+					</div><!--button-->
+
+				</div><!--main-right-content-nd-->
+			</div><!--main-right-content-->
+
+			<div class="main-right-content">
+				<div class="main-right-content-img">
+					<a href="#"><img src="images/nen.jpg"></a>
+				</div><!--main-right-content-img-->
+
+				<div class="main-right-content-nd">
+					<div class="main-right-content-nd-title">
+						<a href="#">Bài viết 1</a>
+					</div><!--main-right-content-nd-title-->
+					<div class="main-right-content-nd-excerpt">
+						<p>rtyuioptyuiop[ytuiopytuioytuioytuio
+						rtyuioptyuiop[ytuiopytuioytuioytuiortyuioptyuiop[ytuiopytuioytuioytuiortyuioptyuiop[ytuiopytuioytuioytuio</p>
+					</div><!--main-right-content-nd-excerpt-->
+					<div class="main-right-button">
+						<button type="submit" class="btn-them"><a href="#">Đọc thêm <i class="fa fa-angle-double-right"></i></a></button>
+					</div><!--button-->
+
+				</div><!--main-right-content-nd-->
+			</div><!--main-right-content-->
+
+		</div><!--blog-main-right-->
+	</div><!--blog-main-->
+	<div style="clear:left"></div>
+
+	<div class="bottom">
+			<p> Copyright &copy; 2018 - Tuyển Giảng</p>
+			<p> Đại học kinh tế - kỹ thuật công nghiệp</p>
+			
+	</div><!--bottom-->
+</body>
+</html>
---
+++ BlogUnelti/js/jmain.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#blog-slider').slick({
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 5000,
+	});
+});
